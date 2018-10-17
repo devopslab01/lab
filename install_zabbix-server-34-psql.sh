7c435dcb4d8e047f3e8052d7109c96fc99fda88a
root <root@u18node01.lab.local>
2018-10-17 11:26:09 +0300

    fix

--- install_zabbix-server-34-psql.sh
+++ install_zabbix-server-34-psql.sh
@@ -9,7 +9,7 @@ echo
 mkdir -p /root/install
 cd /root/install
 
-wget https://repo.zabbix.com/zabbix/3.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.4-1+bionic_all.deb
+if [ ! -f "zabbix-release_3.4-1+bionic_all.deb" ]; then wget https://repo.zabbix.com/zabbix/3.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.4-1+bionic_all.deb; fi
 dpkg -i zabbix-release_3.4-1+bionic_all.deb
 apt update
 
@@ -18,8 +18,19 @@ apt install -y zabbix-frontend-php
 apt install -y postgresql postgresql-contrib
 
 sudo -i PWD=/var/lib/postgresql/ -u postgres psql << EOF
+DROP DATABASE IF EXISTS zabbix;
+EOF
+
+sudo -i PWD=/var/lib/postgresql/ -u postgres psql << EOF
 CREATE USER zabbix;
 ALTER USER zabbix WITH PASSWORD 'zabbixdbpass';
 CREATE DATABASE zabbix OWNER zabbix;
 GRANT ALL PRIVILEGES ON DATABASE zabbix TO zabbix;
 EOF
+
+zcat /usr/share/doc/zabbix-server-pgsql/create.sql.gz | sudo -u postgres psql zabbix
+
+sed -i -e "/DBHost=/s/.*/DBHost=/" /etc/zabbix/zabbix_server.conf
+sed -i -e "/DBPassword=/s/.*/DBPassword=zabbixdbpass/" /etc/zabbix/zabbix_server.conf
+sed -i -e "/DBName=/s/.*/DBName=zabbix/" /etc/zabbix/zabbix_server.conf
+sed -i -e "/DBUser=/s/.*/DBUser=zabbix/" /etc/zabbix/zabbix_server.conf
