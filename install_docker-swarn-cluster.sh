ad17fd72807aa4b614081303898b86c431d525a5
root <root@u18node01.lab.local>
2018-10-18 14:56:24 +0300

    fix

--- install_docker-swarn-cluster.sh
+++ install_docker-swarn-cluster.sh
@@ -8,16 +8,45 @@ echo
 
 node1="u18node01.lab.local"
 node2="192.168.200.202"
-node3="u18node03.lab.local"
+node3="192.168.200.203"
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
+apt-get update
 
 apt-get -y install docker-ce
 
 systemctl --no-pager status docker
 
-ssh super@$node2 "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
-ssh super@$node2 "add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'"
+## -- node2
+ssh super@$node2 "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+ssh super@$node2 "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'"
+ssh super@$node2 "sudo apt-get update"
 
+ssh super@$node2 "sudo apt-get -y install docker-ce"
+
+ssh super@$node2 "sudo systemctl --no-pager status docker"
+
+docker swarm init
+
+cmd_join=`docker swarm join-token manager |grep "docker swarm join"`
+ssh super@$node2 "sudo $cmd_join"
+
+docker node ls
+
+## -- node3
+
+ssh super@$node3 "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+ssh super@$node3 "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'"
+ssh super@$node3 "sudo apt-get update"
+
+ssh super@$node3 "sudo apt-get -y install docker-ce"
+
+ssh super@$node3 "sudo systemctl --no-pager status docker"
+
+docker swarm init
+
+cmd_join=`docker swarm join-token manager |grep "docker swarm join"`
+ssh super@$node3 "sudo $cmd_join"
+
+docker node ls
