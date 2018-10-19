33bb5e1af14d166559f63ad79ac2ed00cacd5648
Devops Lab <devopslabbox@gmail.com>
2018-10-19 22:24:59 +0300

    fix

---
+++ scripts/revert-snapshot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "USAGE: $0 <kvm_host> <kvm_guest> [<kvm_guest>] ..."
+  exit 1
+fi
+
+echo
+echo "--- Revert Snapshot in KVM ---"
+echo
+
+KVM_HOST=`uname -s`
+KVM_GUESTS=`echo $* |tr -s ' '`
+
+echo "KVM HOST:   $KVM_HOST"
+echo "KVM GUESTS: $KVM_GUESTS"
+echo
+
+for GUEST in $KVM_GUESTS; do
+  echo "Reverting snapshot on host $GUEST ..."
+
+  SNAP=`sudo virsh snapshot-list $GUEST |tail -n2 |head -n1 |awk '{print $1}'`
+  echo "Snapshot: $SNAP"
+  sudo virsh snapshot-revert $GUEST $SNAP
+  echo "Start "
+  sudo virsh start $GUEST
+
+  echo "Snapshot reverted."
+done
+
+echo "Done."
+echo
